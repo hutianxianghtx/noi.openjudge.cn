6ad408c50c7bb4b6c6d144b4bb57cd5c59600d8f
胡天翔 <meishihutianxiang@hotmail.com>
2016-10-04 12:17:25 +0800

    Create ID06_空格分隔输出.cpp

---
+++ ID06_空格分隔输出.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+	char a;
+	int b;
+	float c;
+	double d;
+	cin>>a>>b>>c>>d;
+	
+	printf("%c %d %.6f %.6lf\n",a,b,c,d);
+	return 0;
+}
