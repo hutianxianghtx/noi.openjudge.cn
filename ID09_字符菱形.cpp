88488898eee4721c9b3ee6eaa951ed3388b15e7b
胡天翔 <meishihutianxiang@hotmail.com>
2016-10-04 12:18:38 +0800

    Create ID09_字符菱形.cpp

---
+++ ID09_字符菱形.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char c;
+	cin>>c;
+	
+	for(int i=1;i<=3;i++) {
+		for(int j=1;j<=3-i;j++) cout<<' ';
+		for(int j=1;j<=2*i-1;j++) cout<<c;
+		cout<<endl;
+	}
+	
+	for(int i=2;i>=1;i--) {
+		for(int j=1;j<=3-i;j++) cout<<' ';
+		for(int j=1;j<=2*i-1;j++) cout<<c;
+		cout<<endl;
+	}
+	return 0;
+}
