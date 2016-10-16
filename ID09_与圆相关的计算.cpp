9a7f4ed6f0aa0244483efc24aebc411dbd491fe9
胡天翔 <meishihutianxiang@hotmail.com>
2016-10-16 15:55:28 +0800

    Create ID09_与圆相关的计算.cpp

---
+++ ID09_与圆相关的计算.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+	double r;
+	scanf("%lf",&r);
+	
+	printf("%.4lf %.4lf %.4lf\n",2*r,2*3.14159*r,3.14159*r*r);
+	return 0;
+}
