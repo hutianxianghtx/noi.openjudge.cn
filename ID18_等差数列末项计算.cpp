e1a89567c47b837d4a0cc2c285e881be75e03be1
胡天翔 <meishihutianxiang@hotmail.com>
2016-10-16 16:03:35 +0800

    Update ID18_等差数列末项计算.cpp

--- ID18_等差数列末项计算.cpp
+++ ID18_等差数列末项计算.cpp
@@ -4,13 +4,11 @@
 using namespace std;
 
 int main() {
-	double a,b,c;
-	scanf("%lf%lf%lf",&a,&b,&c);
+	float x1,y1,x2,y2,x3,y3;
+	scanf("%f%f%f%f%f%f",&x1,&y1,&x2,&y2,&x3,&y3);
 	
-	if(a+b<=c || a+c<=b || b+c<=a) printf("Data Error\n");
-	else {
-		double r=(a+b+c)/2;
-		printf("%.4lf\n",sqrt(r*(r-a)*(r-b)*(r-c)));
-	}
+	float ans=0.5*abs((x2-x1)*(y3-y1)-(x3-x1)*(y2-y1));
+	
+	printf("%.2f\n",ans);
 	return 0;
 }
